21d52766750b15bd403a0b0ca80e3ef37175c0d5
rolecram <48367010+rolecram@users.noreply.github.com>
2021-06-03 15:21:13 -0300

    first commit

---
+++ first.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+
+#s = {2,3,4}
+s = set([2,3,'marcelo'])
+
+#for e in s:
+#   print(e)
+
+e = next(iter(s))
+e1 = next(iter(s))
+#x = s.pop()
+#x1 = s.pop()
+#x2 = s.pop()
+
+#print (x2[0])
+#print(list(s)[0])
+print (e1)
+
+#response = requests.get("https://jsonplaceholder.typicode.com/todos")
+#todos = json.loads(response.text)
+
