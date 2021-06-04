ec7085de1141c9e34b3c02389f5e822830a9ef91
rolecram <48367010+rolecram@users.noreply.github.com>
2021-06-03 21:09:01 -0300

    api invoke

--- first.py
+++ first.py
@@ -2,22 +2,11 @@ import json
 import requests
 
 
-#s = {2,3,4}
-s = set([2,3,'marcelo'])
+res = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Montreal,,CA&appid=b26f884059504c801d371a4834ce4623&mode=json')
+print(res)
+jres = res.json()
 
-#for e in s:
-#   print(e)
+print (jres['coords'])
 
-e = next(iter(s))
-e1 = next(iter(s))
-#x = s.pop()
-#x1 = s.pop()
-#x2 = s.pop()
 
-#print (x2[0])
-#print(list(s)[0])
-print (e1)
-
-#response = requests.get("https://jsonplaceholder.typicode.com/todos")
-#todos = json.loads(response.text)
 
