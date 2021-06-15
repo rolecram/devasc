7e81d1cecebe859dd300d1d0c7a3b0d0251b901a
Marcelo <48367010+rolecram@users.noreply.github.com>
2021-06-15 20:39:53 -0300

    Update first.py

--- first.py
+++ first.py
@@ -2,12 +2,19 @@ import json
 import requests
 
 #r = requests.get('<MY_URI>', headers={'Authorization': 'Bearer <MY_TOKEN>'})
+#res = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Montreal,,CA&appid=b26f884059504c801d371a4834ce4623&mode=json')
 
-res = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Montreal,,CA&appid=b26f884059504c801d371a4834ce4623&mode=json')
-print(res)
+apiUrl = 'https://webexapis.com/v1/rooms'
+access_token = 'your_access_token'
+httpHeaders = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
+queryParams = { 'sortBy': 'lastactivity', 'max': '2' }
+response = requests.get( url = apiUrl, headers = httpHeaders, params = queryParams )
+print( response.status_code )
+print( response.text )
+
 dic = res.json()
 
-print (dic['coords'])
+print (dic['?'])
 
 
 
