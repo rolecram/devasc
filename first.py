158257a4d822d0839023a4d7ce5b7341c0817fee
Marcelo <48367010+rolecram@users.noreply.github.com>
2021-07-03 18:42:43 -0300

    Update first.py

--- first.py
+++ first.py
@@ -5,7 +5,7 @@ import requests
 #res = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Montreal,,CA&appid=b26f884059504c801d371a4834ce4623&mode=json')
 
 apiUrl = 'https://webexapis.com/v1/rooms'
-access_token = 'your_access_token'
+access_token = 'MjYwNTAxNzMtYTQ4NS00MzZmLTgwYTEtYmJjNmYzZjEyY2VlM2Q0YzNlNjQtMWVk_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
 httpHeaders = { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token }
 queryParams = { 'sortBy': 'lastactivity', 'max': '2' }
 response = requests.get( url = apiUrl, headers = httpHeaders, params = queryParams )
